c572747b9dcc78dfb0f665912709871fe49b1cba
mambax7 <mambax7@gmail.com>
2015-05-05 09:19:13 -0400

    Original files

---
+++ new-crimson/package.ini.php
@@ -0,0 +1,17 @@
+<?php
+/**
+
+[Manifesto]
+Name="New-Crimson"
+Depends=Legacy_RenderSystem
+Url="http://www.cmswave.com/"
+Version="1.00"
+
+[Theme]
+Author=CMSWAVE
+ScreenShot="screenshot.jpg"
+Description="New-Crimson by Default theme of XOOPS Cube 2.1"
+W3C=NG
+
+*/
+?>
